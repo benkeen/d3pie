820c2b8af634681df63fc1fdb28e54056d70f77c
Ben Keen <ben.keen@gmail.com>
2013-12-31 18:55:08 -0800

    More work

--- core/libs/jquery.d3pie.js
+++ core/libs/jquery.d3pie.js
@@ -4,6 +4,10 @@
  * @version 0.1.0
  * @date Dec 2013
  * http://github.com/benkeen/d3pie
+ *
+ * TODO:
+ * - need to allow multiple pies per page
+ * - prefix all classes and allow it to be customized
  */
 ;(function($, window, document) {
 
@@ -73,132 +77,22 @@
 	};
 
 
-	var _arc, _svg;
+	var _arc, _svg, _totalSize, _data;
 	d3pie.prototype.init = function() {
 
 		// store the options in a local var for circumventing any "this" nonsense
 		var options = this.options;
-		var data    = this.options.data;
-		var totalSize = _getTotalPieSize(data);
+		_data      = this.options.data;
+		_totalSize = _getTotalPieSize(_data);
 
-		// temporary - but it should be calculated
+		// temporary
 		options.outerRadius = ((options.width < options.height) ? options.width : options.height) / 3;
 
 		_addSVGSpace(this.element, options);
 		_addTitle(options.title);
-
-
-		_arc = d3.svg.arc()
-			.innerRadius(options.innerRadius)
-			.outerRadius(options.outerRadius)
-			.startAngle(0)
-			.endAngle(function(d) {
-				return (d.value / totalSize) * 2 * Math.PI;
-			});
-
-		var g = _svg.selectAll(".arc")
-			.data(
-				data.filter(function(d) { return d.value; }),
-				function(d) { return d.label; }
-			)
-			.enter()
-			.append("g")
-			.attr("class", "arc");
-
-		g.append("path")
-			.attr("id", function(d, i) {
-				return "segment" + i;
-			})
-			.style("fill", function(d, index) {
-				return options.colors.segments[index];
-			})
-			.transition()
-			.ease("cubic-in-out")
-			.duration(options.effects.loadSpeed)
-			.attrTween("d", _arcTween);
-
-		_svg.selectAll("g")
-			.attr("transform",
-			function(d, i) {
-				var angle = _getSegmentRotationAngle(d, i, data, totalSize);
-				return "rotate(" + angle + ")";
-			}
-		);
-
-		var labelGroup = _svg.selectAll(".arc")
-			.append("g")
-			.attr("class", "labelGroup")
-			.attr("id", function(d, i) {
-				return "labelGroup" + i
-			});
-
-		labelGroup.append("text")
-			.attr("id", function(d, i) {
-				return "label" + i
-			})
-			.text(function(d) {
-				return d.label;
-			})
-			.attr("transform", function(d, i) {
-				var angle = _getSegmentRotationAngle(d, i, data, totalSize);
-				var labelRadius = options.outerRadius + 20;
-				var c = _arc.centroid(d),
-					x = c[0],
-					y = c[1],
-					h = Math.sqrt(x*x + y*y); // pythagorean theorem for hypotenuse
-
-				return "translate(" + (x/h * labelRadius) +  ',' + (y/h * labelRadius) +  ") rotate(" + -angle + ")";
-			})
-			.style('opacity', 0);
-//			.style('font-weight', "bold");
-
-		labelGroup.append("text")
-			.text(function(d) {
-				return Math.round((d.value / totalSize) * 100) + "%";
-			})
-			.attr("class", "pieShare")
-			.attr("transform", function(d, i) {
-				var angle = _getSegmentRotationAngle(d, i, data, totalSize);
-				var labelRadius = options.outerRadius + 20;
-				var c = _arc.centroid(d),
-					x = c[0],
-					y = c[1],
-					h = Math.sqrt(x*x + y*y); // pythagorean theorem for hypotenuse
-
-				return "translate(" + (x/h * labelRadius) +  ',' + (y/h * labelRadius) +  ") rotate(" + -angle + ")";
-			})
-			.attr("dx", function(d, i) {
-				var correspondingLabelWidth = document.getElementById("label" + i).getComputedTextLength();
-				return correspondingLabelWidth + 3;
-			})
-			.style("opacity", 0);
-
-		labelGroup
-			.attr("dx", function(d, i) {
-				return "-200px";
-			});
-
-		// fade in the labels when the load effect is complete
-		setTimeout(function() {
-			g.selectAll("text")
-				.transition()
-				.duration(options.effects.labelFadeInTime)
-				.style("opacity", 1);
-		}, options.effects.loadSpeed);
-
-		$(function() {
-			$(".arc").on("click", function(e) {
-				var $segment = $(e.currentTarget).find("path");
-
-				// detect if it's currently moving here
-
-				if ($segment.attr("class") === "expanded") {
-					_closeSegment($segment[0]);
-				} else {
-					_openSegment($segment[0]);
-				}
-			});
-		});
+		_createPie(this.element, options);
+		_addLabels(options);
+		_addSegmentEventHandlers();
 	};
 
 
@@ -268,27 +162,145 @@
 	var _addSVGSpace = function(element, options) {
 		_svg = d3.select(element).append("svg:svg")
 			.attr("width", options.width)
-			.attr("height", options.height)
-			.append("svg:g")
-			.attr("transform", "translate(" + (options.width/2) + "," + (options.height/2) + ")");
+			.attr("height", options.height);
 	};
 
+	/**
+	 * Adds the Pie Chart title.
+	 * @param titleData
+	 * @private
+	 */
 	var _addTitle = function(titleData) {
-
-		console.log("???");
-
 		var title = _svg.selectAll(".title")
-			.append("g")
-			.attr("id", function(d, i) {
-				return "title";
-			})
-			.attr("dx", 50)
-			.attr("dy", 50);
+			.data([titleData]);
 
-		title.append("text")
-			.text(function(d) {
-				return titleData.title;
+		title.enter()
+			.append("text")
+			.attr("class", "title")
+			.attr("x", 20)
+			.attr("y", 20)
+			.attr("fill", function(d) {
+				return d.color;
 			})
+			.text(function(d) {
+				return d.text;
+			})
+	};
+
+
+	var _createPie = function(element, options) {
+		var pieChartElement = _svg.append("g")
+			.attr("transform", "translate(" + (options.width/2) + "," + (options.height/2) + ")")
+			.attr("class", "pieChart");
+
+		_arc = d3.svg.arc()
+			.innerRadius(options.innerRadius)
+			.outerRadius(options.outerRadius)
+			.startAngle(0)
+			.endAngle(function(d) {
+				return (d.value / _totalSize) * 2 * Math.PI;
+			});
+
+		var g = pieChartElement.selectAll(".arc")
+			.data(
+				_data.filter(function(d) { return d.value; }),
+				function(d) { return d.label; }
+			)
+			.enter()
+			.append("g")
+			.attr("class", "arc");
+
+		g.append("path")
+			.attr("id", function(d, i) {
+				return "segment" + i;
+			})
+			.style("fill", function(d, index) {
+				return options.colors.segments[index];
+			})
+			.transition()
+			.ease("cubic-in-out")
+			.duration(options.effects.loadSpeed)
+			.attrTween("d", _arcTween);
+
+		_svg.selectAll("g.arc")
+			.attr("transform",
+				function(d, i) {
+					var angle = _getSegmentRotationAngle(d, i, _data, _totalSize);
+					return "rotate(" + angle + ")";
+				}
+			);
+	};
+
+	var _addLabels = function(options) {
+		var labelGroup = _svg.selectAll(".arc")
+			.append("g")
+			.attr("class", "labelGroup")
+			.attr("id", function(d, i) {
+				return "labelGroup" + i;
+			});
+
+		labelGroup.append("text")
+			.attr("class", "segmentLabel")
+			.attr("id", function(d, i) {
+				return "label" + i;
+			})
+			.text(function(d) {
+				return d.label;
+			})
+			.attr("transform", function(d, i) {
+				var angle = _getSegmentRotationAngle(d, i, _data, _totalSize);
+				var labelRadius = options.outerRadius + 20;
+				var c = _arc.centroid(d),
+					x = c[0],
+					y = c[1],
+					h = Math.sqrt(x*x + y*y); // pythagorean theorem for hypotenuse
+
+				return "translate(" + (x/h * labelRadius) +  ',' + (y/h * labelRadius) +  ") rotate(" + -angle + ")";
+			})
+			.style('opacity', 0)
+			.style('font-size', "8pt");
+
+		labelGroup.append("text")
+			.text(function(d) {
+				return Math.round((d.value / _totalSize) * 100) + "%";
+			})
+			.attr("class", "pieShare")
+			.attr("transform", function(d, i) {
+				var angle = _getSegmentRotationAngle(d, i, _data, _totalSize);
+				var labelRadius = options.outerRadius + 20;
+				var c = _arc.centroid(d),
+					x = c[0],
+					y = c[1],
+					h = Math.sqrt(x*x + y*y); // pythagorean theorem for hypotenuse
+
+				return "translate(" + (x/h * labelRadius) +  ',' + (y/h * labelRadius) +  ") rotate(" + -angle + ")";
+			})
+			.attr("dx", function(d, i) {
+				var correspondingLabelWidth = document.getElementById("label" + i).getComputedTextLength();
+				return correspondingLabelWidth + 3;
+			})
+			.style("opacity", 0);
+
+		// fade in the labels when the load effect is complete
+		setTimeout(function() {
+			d3.selectAll("text.segmentLabel")
+				.transition()
+				.duration(options.effects.labelFadeInTime)
+				.style("opacity", 1);
+		}, options.effects.loadSpeed);
+	};
+
+	var _addSegmentEventHandlers = function() {
+		$(".arc").on("click", function(e) {
+			var $segment = $(e.currentTarget).find("path");
+
+			// detect if it's currently moving here
+			if ($segment.attr("class") === "expanded") {
+				_closeSegment($segment[0]);
+			} else {
+				_openSegment($segment[0]);
+			}
+		});
 	};
 
 })(jQuery, window, document);
