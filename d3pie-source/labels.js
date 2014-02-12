d8c299d326be8fc56d8fdc0c4950c1438cf0e383
Ben Keen <ben.keen@gmail.com>
2014-02-11 20:12:58 -0800

    label fonts

--- d3pie-source/labels.js
+++ d3pie-source/labels.js
@@ -28,6 +28,7 @@ d3pie.labels = {
 			.attr("id", function(d, i) { return "label" + i; })
 			.text(function(d) { return d.label; })
 			.style("font-size", _options.labels.mainLabel.fontSize)
+			.style("font-family", _options.labels.mainLabel.font)
 			.style("fill", _options.labels.mainLabel.color)
 			.style("opacity", 0);
 
