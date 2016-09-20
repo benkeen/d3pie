a8295232451dea336cb860c19348c28d4a863bb0
Hai-Nam Nguyen <jcisio@gmail.com>
2016-09-20 11:32:57 +0200

    Compatibility with D3js v4

--- d3pie/d3pie.js
+++ d3pie/d3pie.js
@@ -1305,7 +1305,7 @@ var segments = {
 			.attr("transform", function() { return math.getPieTranslateCenter(pieCenter); })
 			.attr("class", pie.cssPrefix + "pieChart");
 
-		var arc = d3.svg.arc()
+		var arc = d3.arc()
 			.innerRadius(pie.innerRadius)
 			.outerRadius(pie.outerRadius)
 			.startAngle(0)
@@ -1337,7 +1337,7 @@ var segments = {
 			.style("stroke", segmentStroke)
 			.style("stroke-width", 1)
 			.transition()
-			.ease("cubic-in-out")
+			.ease(d3.easeCubicInOut)
 			.duration(loadSpeed)
 			.attr("data-index", function(d, i) { return i; })
 			.attrTween("d", function(b) {
